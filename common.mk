f5b230a40d7364fa62a39d7731b6c030d93d4237
Alex Sarraf <asarraf@codeaurora.org>
2020-06-11 17:46:11 -0700

    sm6225-common: props: Disable SF client composition cache
    Disable SF client composition cache. Helps with framework
    client target fence management.
    
    Change-Id: I73e03e76f51d00fafad38479be00f4005be50665
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -204,6 +204,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.disable_backpressure=1 \
+    debug.sf.disable_client_composition_cache=1 \
     debug.renderengine.backend=skiaglthreaded \
     ro.vendor.display.sensortype=2 \
     vendor.display.idle_time=0 \
