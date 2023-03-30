c5c97dcaeda21437c75839d3c949ecb94946d9e2
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-03-30 17:23:30 +0000

    sm6225-common: Update fastboot impl-mock to @1.1
    Change-Id: I917f237becdc63814e77971ccf68eb81d988ead4
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -231,7 +231,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastboot
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
 # Fingerprint
