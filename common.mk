b368776748debce598416ca5e2c0a81efd426cf5
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-04 05:05:53 +0000

    sm6225-common: init: Import override vendor.qti.hardware.charger_monitor@1.0-service.rc from qcom/common
    Change-Id: I7a70243af7863e9ae37bb7de5f99751b54c407f4
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -170,6 +170,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
 
 # Charger
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/vendor.qti.hardware.charger_monitor@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.charger_monitor@1.0-service.rc
+
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.quick.charge=1 \
     ro.charger.disable_init_blank=true
