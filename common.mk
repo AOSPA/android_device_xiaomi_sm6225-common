24baab1d6c6cc42e90a3e1be0fd17e5f21072186
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-06-23 09:47:06 +0700

    sm6225-common: Decommonize fingerprint service
    [1] - Citrus e Lime use prebuilt
    
    Change-Id: Icb6b269c1d1a98c9556c970c600a48c5b7882472

--- common.mk
+++ common.mk
@@ -251,9 +251,6 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
