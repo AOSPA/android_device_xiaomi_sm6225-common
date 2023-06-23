eb8adef0d50197af52ebe10eef6dff5619309def
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-06-23 09:26:16 +0700

    sm6225-common: Add citrus and lime support
    [1] - SM6115
    
    Change-Id: I1b629a66eba5b530204cc0f64501de207d338392

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,7 +24,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "fog spes"
+write_headers "citrus fog lime spes"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
