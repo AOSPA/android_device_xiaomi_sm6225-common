3629387967025b546dd9c7940ad11ecc5dfd2fc8
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-06 22:33:39 +0000

    Revert "sm6225-common: init: Fix fingerprint goodix events"
    This reverts commit 46add643b956250d15c0e3c7963bd1b881f635db.
    
    [1] - Fixed in kernel side
    
    Change-Id: Ic313d48dd3464db2de976a400c4765f35ad8d594

--- init/init.goodix.events.sh
+++
@@ -1,9 +0,0 @@
-#! /vendor/bin/sh
-EVENT_NAME=$(tr "\n" " " < /proc/bus/input/devices | \
-grep -o "uinput-goodix.*" | \
-grep -Eo "event[0-9]+" | \
-head -1)
-
-[ -z "$EVENT_NAME" ] && exit
-rm /dev/input/$EVENT_NAME
-
