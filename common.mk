76d393d64fb07dd1af62a71960a16c65255e11cf
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-10-15 19:10:44 +0000

    sm6225-common: Remove unecessary vndk definition
    Change-Id: Ic5fb2414c86496dc3affeaf6f74f9e606e486eb8
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -458,9 +458,6 @@ endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# VNDK
-PRODUCT_EXTRA_VNDK_VERSIONS := 30
-
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
