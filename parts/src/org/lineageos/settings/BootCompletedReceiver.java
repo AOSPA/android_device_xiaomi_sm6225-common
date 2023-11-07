01878ffd3a4c45edfabc00d634a38686cd55d520
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-07 08:12:46 +0530

    sm6225-common: parts: Start services as early as possible on bootup
    BOOT_COMPLETED intent is broadcast only after the device is unlocked and
    user data (encrypted storage) is available. Use LOCKED_BOOT_COMPLETED
    intent for services that don't require accessing data.
    
    While we're at it, target latest SDK and uprev the package.
    
    Change-Id: I067e944892607ff0d38f503d8e1465a0081e0648
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -33,16 +33,25 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            return;
+        Log.i(TAG, "Received intent: " + intent.getAction());
+
+        switch (intent.getAction()) {
+            case Intent.ACTION_LOCKED_BOOT_COMPLETED:
+                onLockedBootCompleted(context);
+                break;
+            case Intent.ACTION_BOOT_COMPLETED:
+                onBootCompleted(context);
+                break;
         }
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        try {
-            DiracUtils.getInstance(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
-        ThermalUtils.initialize(context);
     }
+
+    private static void onLockedBootCompleted(Context context) {
+        // Services that don't require reading from data.
+    }
+    
+    private static void onBootCompleted(Context context) {
+        // Data is now accessible (user has just unlocked).
+        DiracUtils.getInstance(context);
+        ThermalUtils.initialize(context);
+    }        
 }
