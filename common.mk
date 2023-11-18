c408ef44ec964947602c8ee8be06815827297591
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-11-18 01:47:20 +0000

    sm6225-common: Use AIDL QTI HEALTH hal
    Change-Id: Ia1f2b119ceb8f587c7794e90ddf43e614964c63f
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -161,10 +161,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/vendor.qti.hardware.charger_monitor@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.charger_monitor@1.0-service.rc
 
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.vendor.quick.charge=1 \
-    ro.charger.disable_init_blank=true
-
 # Crypto
 PRODUCT_VENDOR_PROPERTIES += \
     ro.crypto.allow_encrypt_override=true \
@@ -355,6 +351,8 @@ TARGET_COMMON_QTI_COMPONENTS += \
     vibrator \
     wfd \
     wlan
+
+TARGET_USE_AIDL_QTI_HEALTH := true
 
 # Radio
 PRODUCT_PRODUCT_PROPERTIES += \
