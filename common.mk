11be95d8006a364ddac3448667c6396624005789
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-01-30 16:41:04 +0000

    sm6225-common: perf: Import perfconfigstore from qcomn/common again
    Change-Id: If466b8577da2e4ff8ac41afe3342ae0967e98515
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -315,6 +315,10 @@ PRODUCT_PACKAGES += \
     BengalWifiOverlay \
     WifiMainline
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
