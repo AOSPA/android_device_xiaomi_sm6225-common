78332a34f2e3105dc85d7ec2a90c85befa4148b8
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-08-01 15:42:29 +0000

    sm6225-common: Remove libhwbinder dependency
    Change-Id: I01d79eb24be113801fa8ff25727814b77f0052db
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -57,6 +57,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/hw/vendor.qti.hardware.soter@1.0-impl.so)
+            "${PATCHELF}" --remove-needed libhwbinder.so "${2}"
+            ;;
+    esac
+}
+
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
