46ad534027f64152d249601848b5a269a5da32c3
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-09-26 17:20:04 -0300

    sm6225-common: Drop media-legacy component
    Change-Id: I9edf174735482bb3cda308865623b08f15e62891
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- Android.mk
+++ Android.mk
@@ -19,5 +19,8 @@ ifneq ($(filter citrus fog lime spes,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := fastcv_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := device/qcom/common/vendor/media-legacy/include
+include $(BUILD_HEADER_LIBRARY)
 
 endif
