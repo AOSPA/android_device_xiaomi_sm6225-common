e5a5b7781ce16310c907cf11953a0fe9496b86f3
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-10-11 21:03:26 +0000

    WIP
    Change-Id: I1e66791171f7cddc0db165fe7c6ad0c362f85b2f
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,6 +22,57 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+        "device/xiaomi/sm6225-common",
+        "hardware/qcom/display",
+        "hardware/qcom/display/gralloc",
+        "hardware/qcom/display/libdebug",
+        "hardware/xiaomi"
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+    case "$1" in
+            com.qualcomm.qti.dpm.api@1.0 | \
+            com.qualcomm.qti.imscmservice@2.0 | \
+            com.qualcomm.qti.imscmservice@2.1 | \
+            com.qualcomm.qti.imscmservice@2.2 | \
+            com.qualcomm.qti.uceservice@2.0 | \
+            com.qualcomm.qti.uceservice@2.1 | \
+            com.qualcomm.qti.uceservice@2.2 | \
+            com.qualcomm.qti.uceservice@2.3 | \
+            libmmosal | \
+            vendor.qti.data.factory@2.0 | \
+            vendor.qti.data.factory@2.1 | \
+            vendor.qti.data.factory@2.2 | \
+            vendor.qti.data.factory@2.3 | \
+            vendor.qti.data.mwqem@1.0 | \
+            vendor.qti.data.slm@1.0 | \
+            vendor.qti.diaghal@1.0 | \
+            vendor.qti.hardware.wifidisplaysession@1.0 | \
+            vendor.qti.imsrtpservice@3.0)
+            echo "$1_vendor"
+            ;;
+            libOmxCore | \
+            libwpa_client)
+            # Android.mk only packages
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+    lib_to_package_fixup_proto_3_9_1 "$1" ||
+    lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
