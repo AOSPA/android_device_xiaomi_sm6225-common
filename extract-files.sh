df7e7bd00204b5be6684e7034f876b0fb0eb207f
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-10-11 21:03:26 +0000

    WIP
    Change-Id: I1e66791171f7cddc0db165fe7c6ad0c362f85b2f
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -64,12 +64,8 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib/hw/*.so | vendor/lib/*.so | vendor/lib64/hw/*.so | vendor/lib64/*.so)
+        vendor/lib64/mediadrm/libwvdrmengine.so|vendor/lib64/libwvhidl.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --remove-needed libhwbinder.so "${2}"
-            "${PATCHELF}" --remove-needed libhidltransport.so "${2}"
-            ;;
-        vendor/lib64/mediadrm/libwvdrmengine.so | vendor/lib64/libwvhidl.so)
             grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
         vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
