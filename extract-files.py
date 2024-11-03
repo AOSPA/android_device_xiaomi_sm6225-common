3f0a1f2afa49ced215496c8e4e5d5beaf6383acf
Michael Bestas <mkbestas@lineageos.org>
2024-11-03 07:33:51 +0200

    sm6225-common: Switch to python extract-utils
    Change-Id: Ida4b83459d9739ac880b26a91df974e1db8423e9
    Signed-off-by: GitHub <noreply@github.com>

---
+++ extract-files.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+from extract_utils.fixups_blob import blob_fixup, blob_fixups_user_type
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import ExtractUtils, ExtractUtilsModule
+
+namespace_imports = [
+    'hardware/qcom/display',
+    'hardware/qcom/display/gralloc',
+    'hardware/qcom/display/libdebug',
+    'hardware/qcom/display/sde-drm',
+    'hardware/xiaomi',
+    'vendor/qcom/common/vendor/display',
+    'vendor/qcom/common/vendor/display/4.19',
+    'vendor/qcom/common/vendor/gps-legacy',
+    'vendor/qcom/common/vendor/keymaster',
+    'vendor/qcom/common/vendor/media-legacy',
+    'vendor/qcom/common/vendor/wlan-legacy',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'com.qualcomm.qti.imscmservice@2.0',
+        'com.qualcomm.qti.imscmservice@2.1',
+        'com.qualcomm.qti.imscmservice@2.2',
+        'com.qualcomm.qti.uceservice@2.0',
+        'com.qualcomm.qti.uceservice@2.1',
+        'com.qualcomm.qti.uceservice@2.2',
+        'com.qualcomm.qti.uceservice@2.3',
+        'libmmosal',
+        'vendor.qti.data.factory@2.0',
+        'vendor.qti.data.factory@2.1',
+        'vendor.qti.data.factory@2.2',
+        'vendor.qti.data.factory@2.3',
+        'vendor.qti.data.mwqem@1.0',
+        'vendor.qti.data.slm@1.0',
+        'vendor.qti.hardware.data.cne.internal.api@1.0',
+        'vendor.qti.hardware.data.cne.internal.constants@1.0',
+        'vendor.qti.hardware.data.cne.internal.server@1.0',
+        'vendor.qti.hardware.data.connection@1.0',
+        'vendor.qti.hardware.data.connection@1.1',
+        'vendor.qti.hardware.data.dynamicdds@1.0',
+        'vendor.qti.hardware.data.iwlan@1.0',
+        'vendor.qti.hardware.data.latency@1.0',
+        'vendor.qti.hardware.data.lce@1.0',
+        'vendor.qti.hardware.data.qmi@1.0',
+        'vendor.qti.hardware.embmssl@1.0',
+        'vendor.qti.hardware.embmssl@1.1',
+        'vendor.qti.hardware.mwqemadapter@1.0',
+        'vendor.qti.hardware.radio.am@1.0',
+        'vendor.qti.hardware.radio.atcmdfwd@1.0',
+        'vendor.qti.hardware.radio.ims@1.0',
+        'vendor.qti.hardware.radio.ims@1.1',
+        'vendor.qti.hardware.radio.ims@1.2',
+        'vendor.qti.hardware.radio.ims@1.3',
+        'vendor.qti.hardware.radio.ims@1.4',
+        'vendor.qti.hardware.radio.ims@1.5',
+        'vendor.qti.hardware.radio.ims@1.6',
+        'vendor.qti.hardware.radio.ims@1.7',
+        'vendor.qti.hardware.radio.ims@1.8',
+        'vendor.qti.hardware.radio.ims@1.9',
+        'vendor.qti.hardware.radio.internal.deviceinfo@1.0',
+        'vendor.qti.hardware.radio.lpa@1.0',
+        'vendor.qti.hardware.radio.lpa@1.1',
+        'vendor.qti.hardware.radio.lpa@1.2',
+        'vendor.qti.hardware.radio.qcrilhook@1.0',
+        'vendor.qti.hardware.radio.qtiradio@1.0',
+        'vendor.qti.hardware.radio.qtiradio@2.0',
+        'vendor.qti.hardware.radio.qtiradio@2.1',
+        'vendor.qti.hardware.radio.qtiradio@2.2',
+        'vendor.qti.hardware.radio.qtiradio@2.3',
+        'vendor.qti.hardware.radio.qtiradio@2.4',
+        'vendor.qti.hardware.radio.qtiradio@2.5',
+        'vendor.qti.hardware.radio.qtiradio@2.6',
+        'vendor.qti.hardware.radio.qtiradio@2.7',
+        'vendor.qti.hardware.radio.uim@1.0',
+        'vendor.qti.hardware.radio.uim@1.1',
+        'vendor.qti.hardware.radio.uim@1.2',
+        'vendor.qti.hardware.radio.uim_remote_client@1.0',
+        'vendor.qti.hardware.radio.uim_remote_client@1.1',
+        'vendor.qti.hardware.radio.uim_remote_client@1.2',
+        'vendor.qti.hardware.radio.uim_remote_server@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.ims.callcapability@1.0',
+        'vendor.qti.ims.callinfo@1.0',
+        'vendor.qti.ims.configservice@1.0',
+        'vendor.qti.ims.configservice@1.1',
+        'vendor.qti.ims.connection@1.0',
+        'vendor.qti.ims.factory@1.0',
+        'vendor.qti.ims.factory@1.1',
+        'vendor.qti.ims.factory@2.0',
+        'vendor.qti.ims.factory@2.1',
+        'vendor.qti.ims.factory@2.2',
+        'vendor.qti.ims.rcsconfig@1.0',
+        'vendor.qti.ims.rcsconfig@1.1',
+        'vendor.qti.ims.rcsconfig@2.0',
+        'vendor.qti.ims.rcsconfig@2.1',
+        'vendor.qti.ims.rcssip@1.0',
+        'vendor.qti.ims.rcssip@1.1',
+        'vendor.qti.ims.rcssip@1.2',
+        'vendor.qti.ims.rcsuce@1.0',
+        'vendor.qti.ims.rcsuce@1.1',
+        'vendor.qti.ims.rcsuce@1.2',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.latency@2.0',
+        'vendor.qti.latency@2.1',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libOmxCore',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm6225-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
