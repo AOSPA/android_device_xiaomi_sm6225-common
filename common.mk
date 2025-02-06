a6ea06f303b46ea2233b29df6d52b0bf26bee4d2
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    sm6225-common: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -51,6 +51,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery \
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
